64e027962fbc6b16805ebc0b1521017956ca9121
Roy Smart <roytsmart@gmail.com>
2024-09-11 20:59:50 -0600

    Added an `sg` module to collect spectrographic analyses. (#7)

--- docs/index.rst
+++ docs/index.rst
@@ -47,24 +47,10 @@ and display as a false-color movie.
     import iris
 
     # Download a 320-step raster sequence
-    obs = iris.SpectrographObservation.from_time_range(
-        time_start=astropy.time.Time("2021-09-23T02:00"),
-        time_stop=astropy.time.Time("2021-09-23T03:00"),
+    obs = iris.sg.SpectrographObservation.from_time_range(
+        time_start=astropy.time.Time("2017-02-11T04:50"),
+        time_stop=astropy.time.Time("2017-02-11T05:00"),
     )
-
-    # Crop the observation so readthedocs doesn't run out of memory.
-    # This is probably unnecessary on your machine
-    index = {obs.axis_wavelength: slice(75, 150)}
-    obs = dataclasses.replace(
-        obs,
-        inputs=na.TemporalSpectralPositionalVectorArray(
-            time=obs.inputs.time,
-            wavelength=obs.inputs.wavelength[index],
-            position=obs.inputs.position[index],
-        ),
-        outputs=obs.outputs[index],
-    )
-
 
     # Calculate the mean rest wavelength of the
     # brightest spectral line
--- iris/sg/_spectrograph_test.py
+++ iris/sg/_spectrograph_test.py
@@ -6,7 +6,7 @@ import iris
 @pytest.mark.parametrize(
     argnames="array",
     argvalues=[
-        iris.SpectrographObservation.from_time_range(
+        iris.sg.SpectrographObservation.from_time_range(
             time_start=astropy.time.Time("2021-09-23T06:00"),
             time_stop=astropy.time.Time("2021-09-23T07:00"),
         )
@@ -14,14 +14,14 @@ import iris
 )
 class TestSpectrographObservation:
 
-    def test_axis_time(self, array: iris.SpectrographObservation):
+    def test_axis_time(self, array: iris.sg.SpectrographObservation):
         assert isinstance(array.axis_time, str)
 
-    def test_axis_wavelength(self, array: iris.SpectrographObservation):
+    def test_axis_wavelength(self, array: iris.sg.SpectrographObservation):
         assert isinstance(array.axis_wavelength, str)
 
-    def test_axis_detector_x(self, array: iris.SpectrographObservation):
+    def test_axis_detector_x(self, array: iris.sg.SpectrographObservation):
         assert isinstance(array.axis_detector_x, str)
 
-    def test_axis_detector_y(self, array: iris.SpectrographObservation):
+    def test_axis_detector_y(self, array: iris.sg.SpectrographObservation):
         assert isinstance(array.axis_detector_y, str)
